11aee300e5139854a1b5de5845dd7796dd7512cf
Athos Francisco Pereira do Carmo <athos.carmo@mcmec.mcmmontagens.com.br>
2018-12-24 15:59:51 -0300

    Aula de Json

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the FeedPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  public objeto_feed = {
+    titulo: "Athos Francisco do Código",
+    data: "November 5, 1955",
+    descricao: "Estou criando um app incrivel...",
+    qntd_likes: 12,
+    qntd_comments: 4,
+    time_comment: "11h ago"
+  };
+
 
   public nome_usuario: string = "Athos Francisco";
 
